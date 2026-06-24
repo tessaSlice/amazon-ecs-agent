83b21891c1322a827b97863bc2bd1d04f4e500bb
Amy Lee <amytoz@amazon.com>
2026-06-24 21:44:24 +0000

    Add dcgm-init/vendor to git tracking
    Vendor directory committed to match repo convention (agent/, ecs-init/
    both have their vendor directories committed).

---
+++ dcgm-init/vendor/github.com/aws/amazon-ecs-agent/ecs-agent/tcs/model/ecstcs/types.go
@@ -0,0 +1,138 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//	http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package ecstcs
+
+import (
+	"errors"
+	"strings"
+	"time"
+
+	"github.com/aws/amazon-ecs-agent/ecs-agent/utils"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+)
+
+// NewPublishMetricsRequest creates a PublishMetricsRequest object.
+func NewPublishMetricsRequest(instanceMetrics *InstanceMetrics, metadata *MetricsMetadata, taskMetrics []*TaskMetric) *PublishMetricsRequest {
+	return &PublishMetricsRequest{
+		InstanceMetrics: instanceMetrics,
+		Metadata:        metadata,
+		TaskMetrics:     taskMetrics,
+		Timestamp:       (*utils.Timestamp)(aws.Time(time.Now())),
+	}
+}
+
+// NewPublishHealthMetricsRequest creates a PublishHealthRequest
+func NewPublishHealthMetricsRequest(metadata *HealthMetadata, healthMetrics []*TaskHealth) *PublishHealthRequest {
+	return &PublishHealthRequest{
+		Metadata:  metadata,
+		Tasks:     healthMetrics,
+		Timestamp: (*utils.Timestamp)(aws.Time(time.Now())),
+	}
+}
+
+type TelemetryMessage struct {
+	InstanceMetrics *InstanceMetrics
+	Metadata        *MetricsMetadata
+	TaskMetrics     []*TaskMetric
+}
+
+type HealthMessage struct {
+	Metadata      *HealthMetadata
+	HealthMetrics []*TaskHealth
+}
+
+// InstanceStatusMessage represents a message containing instance health status
+// information to be published to the TCS backend.
+type InstanceStatusMessage struct {
+	// Metadata contains identifying information about the container instance
+	// including cluster name, container instance ARN, and request ID.
+	Metadata *InstanceStatusMetadata `json:"metadata,omitempty"`
+
+	// Statuses contains a collection of instance status checks that represent
+	// the health state of various components on the container instance.
+	Statuses []*InstanceStatus `json:"statuses,omitempty"`
+}
+
+const (
+	InstanceHealthCheckTypeContainerRuntime   = "ContainerRuntime"
+	InstanceHealthCheckTypeAgent              = "Agent"
+	InstanceHealthCheckTypeEBSDaemon          = "EBSDaemon"
+	InstanceHealthCheckTypeAcceleratedCompute = "ACCELERATED_COMPUTE"
+)
+
+const (
+	// HealthcheckStatusInitializing is the zero state of a healthcheck status.
+	InstanceHealthCheckStatusInitializing InstanceHealthCheckStatus = iota
+	// HealthcheckStatusOk represents a healthcheck with a true/success result.
+	InstanceHealthCheckStatusOk
+	// HealthcheckStatusImpaired represents a healthcheck with a false/fail result.
+	InstanceHealthCheckStatusImpaired
+	// HealthcheckStatusInsufficientData represents a healthcheck where status cannot be determined.
+	InstanceHealthCheckStatusInsufficientData
+)
+
+// InstanceHealthCheckStatus is an enumeration of possible instance health check statuses.
+type InstanceHealthCheckStatus int32
+
+var instanceHealthCheckStatusMap = map[string]InstanceHealthCheckStatus{
+	"INITIALIZING":      InstanceHealthCheckStatusInitializing,
+	"OK":                InstanceHealthCheckStatusOk,
+	"IMPAIRED":          InstanceHealthCheckStatusImpaired,
+	"INSUFFICIENT_DATA": InstanceHealthCheckStatusInsufficientData,
+}
+
+// String returns a human readable string representation of this object.
+func (hs InstanceHealthCheckStatus) String() string {
+	for k, v := range instanceHealthCheckStatusMap {
+		if v == hs {
+			return k
+		}
+	}
+	// We shouldn't see this.
+	return "NONE"
+}
+
+// Ok returns true if the instance health check status is OK or INITIALIZING.
+func (hs InstanceHealthCheckStatus) Ok() bool {
+	return hs == InstanceHealthCheckStatusOk || hs == InstanceHealthCheckStatusInitializing
+}
+
+// UnmarshalJSON overrides the logic for parsing the JSON-encoded InstanceHealthCheckStatus data.
+func (hs *InstanceHealthCheckStatus) UnmarshalJSON(b []byte) error {
+	if strings.ToLower(string(b)) == "null" {
+		*hs = InstanceHealthCheckStatusInitializing
+		return nil
+	}
+	if b[0] != '"' || b[len(b)-1] != '"' {
+		*hs = InstanceHealthCheckStatusInitializing
+		return errors.New("instance health check status unmarshal: status must be a string or null; Got " + string(b))
+	}
+
+	stat, ok := instanceHealthCheckStatusMap[string(b[1:len(b)-1])]
+	if !ok {
+		*hs = InstanceHealthCheckStatusInitializing
+		return errors.New("instance health check status unmarshal: unrecognized status")
+	}
+	*hs = stat
+	return nil
+}
+
+// MarshalJSON overrides the logic for JSON-encoding the InstanceHealthCheckStatus type.
+func (hs *InstanceHealthCheckStatus) MarshalJSON() ([]byte, error) {
+	if hs == nil {
+		return nil, nil
+	}
+	return []byte(`"` + hs.String() + `"`), nil
+}
---
+++ dcgm-init/vendor/github.com/aws/amazon-ecs-agent/ecs-agent/utils/utils.go
@@ -0,0 +1,181 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//	http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+package utils
+
+import (
+	"encoding/json"
+	"math"
+	"reflect"
+	"regexp"
+	"strconv"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"golang.org/x/exp/constraints"
+)
+
+const httpsPrefix = "https://"
+
+var schemeRegex = regexp.MustCompile(`^(http|https)://`)
+
+func ZeroOrNil(obj interface{}) bool {
+	value := reflect.ValueOf(obj)
+	if !value.IsValid() {
+		return true
+	}
+	if obj == nil {
+		return true
+	}
+	switch value.Kind() {
+	case reflect.Slice, reflect.Array, reflect.Map:
+		return value.Len() == 0
+	}
+	zero := reflect.Zero(reflect.TypeOf(obj))
+	if !value.Type().Comparable() {
+		return false
+	}
+	if obj == zero.Interface() {
+		return true
+	}
+	return false
+}
+
+// Uint16SliceToStringSlice converts a slice of type uint16 to a slice of type
+// string. It uses strconv.Itoa on each element
+func Uint16SliceToStringSlice(slice []uint16) []string {
+	stringSlice := make([]string, len(slice))
+	for i, el := range slice {
+		str := strconv.Itoa(int(el))
+		stringSlice[i] = str
+	}
+	return stringSlice
+}
+
+// Int32PtrToIntPtr converts a *int32 to *int.
+func Int32PtrToIntPtr(int32ptr *int32) *int {
+	if int32ptr == nil {
+		return nil
+	}
+	return aws.Int(int(aws.ToInt32(int32ptr)))
+}
+
+// Int64PtrToInt32Ptr converts a *int64 to *int32.
+func Int64PtrToInt32Ptr(int64ptr *int64) *int32 {
+	if int64ptr == nil {
+		return nil
+	}
+	return aws.Int32(int32(aws.ToInt64(int64ptr)))
+}
+
+// MaxNum returns the maximum value between two numbers.
+func MaxNum[T constraints.Integer | constraints.Float](a, b T) T {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// If the URL doesn't start with "http://" or "https://",
+// prepends "https://" to the URL.
+// Empty strings are returned as-is without modification
+func AddScheme(endpoint string) string {
+	if endpoint == "" {
+		return endpoint
+	}
+
+	if schemeRegex.MatchString(endpoint) {
+		return endpoint
+	}
+	return httpsPrefix + endpoint
+}
+
+// type alias to workaround serializing time.Time into seconds UTC instead of RFC3339
+// example
+// (original) timestamp: "2025-05-09T14:47:58.031Z"
+// (customized) timestamp: "1746802078.031"
+type Timestamp time.Time
+
+// FormatTime returns a string value of the time.
+// https://github.com/aws/aws-sdk-go/blob/main/private/protocol/timestamp.go#L55-L69
+func FormatTime(t time.Time) string {
+	t = t.UTC().Truncate(time.Millisecond)
+
+	ms := t.UnixNano() / int64(time.Millisecond)
+	return strconv.FormatFloat(float64(ms)/1e3, 'f', -1, 64)
+}
+
+// ParseTime attempts to parse the time given the format. Returns
+// the time if it was able to be parsed, and fails otherwise.
+// https://github.com/aws/aws-sdk-go/blob/main/private/protocol/timestamp.go#L73-L101
+func ParseTime(value string) (time.Time, error) {
+	v, err := strconv.ParseFloat(value, 64)
+	_, dec := math.Modf(v)
+	dec = math.Round(dec*1e3) / 1e3 //Rounds 0.1229999 to 0.123
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return time.Unix(int64(v), int64(dec*(1e9))).In(time.UTC), nil
+}
+
+// Follow aws-sdk-go (v1) behavior for time.Time serialization into UTC seconds (UnixTimeFormatName)
+// https://github.com/aws/aws-sdk-go/blob/main/private/protocol/timestamp.go#L54-L69
+func (t *Timestamp) MarshalJSON() ([]byte, error) {
+	if t == nil {
+		return []byte("null"), nil
+	}
+
+	return []byte(FormatTime(time.Time(*t))), nil
+}
+
+// Follow aws-sdk-go (v1) behavior for time.Time deserialization into UTC seconds (UnixTimeFormatName)
+// https://github.com/aws/aws-sdk-go/blob/main/private/protocol/timestamp.go#L90-L97
+func (t *Timestamp) UnmarshalJSON(data []byte) error {
+	value := string(data)
+	if value == "null" {
+		return nil
+	}
+
+	timestamp, err := ParseTime(value)
+	*t = Timestamp(timestamp)
+	return err
+}
+
+// ToPtrSlice converts a slices of values to a slice of pointers to those values.
+func ToPtrSlice[V any](xs []V) []*V {
+	var result []*V
+	for _, x := range xs {
+		result = append(result, &x)
+	}
+	return result
+}
+
+// JsonBlockToStringToStringMap converts a JSON block string to a string-to-string map.
+func JsonBlockToStringToStringMap(jsonBlock string) (map[string]string, error) {
+	out := map[string]string{}
+	err := json.Unmarshal([]byte(jsonBlock), &out)
+	return out, err
+}
+
+// CredentialsIDLogTruncationLen is the number of characters to retain when
+// logging a credentials ID. The full ID is sensitive (bearer token for TMDS).
+const CredentialsIDLogTruncationLen = 8
+
+// TruncateString truncates a string to the specified length and appends
+// "-REDACTED" to indicate the value has been redacted.
+func TruncateString(s string, length int) string {
+	if len(s) <= length {
+		return s
+	}
+	return s[:length] + "-REDACTED"
+}
